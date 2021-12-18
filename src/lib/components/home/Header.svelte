ea6403808aa05df8dfb2657a4c1d112ae17242a8
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-12-18 19:41:16 +0000

    :sparkles: Add some text and a button for further navigation
    By adding text, we indicate to the user, the name of the website that
    they are on. The button will lead to a more complete page that is
    constructed mostly using Three.js and WebGL.

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
 </script>
 
-<header class="flex flex-row content-center h-[5%] p-3">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		class="w-6 h-6 text-white"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
+<header class="flex flex-row items-center h-[5%] p-5 text-white">
+	<div class="flex items-center p-2 mr-auto">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="w-6 h-6 text-white"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+		<p class="ml-2 text-2xl">Stellar</p>
+	</div>
+	<button
+		class="p-2 duration-500 border-b hover:bg-white hover:text-black hover:rounded-md"
 	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-		/>
-	</svg>
+		Enter the solar system &rarr;
+	</button>
 </header>
