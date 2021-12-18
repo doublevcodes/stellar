eb170843dd6d33398b02c01d9b5eed1536613865
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-12-18 20:04:32 +0000

    :sparkles: Add :earth_americas: to the landing page
    This commit introduces the Earth to the home page of the website. No
    comments needed.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Header from '$lib/components/home/Header.svelte';
+	import Earth from '$lib/components/home/Earth.svelte';
 </script>
 
-<main class="h-screen bg-gradient-to-b from-slate-900 to-blue-900">
+<main class="h-screen text-white bg-black">
 	<Header />
+	<div class="grid grid-cols-2 grid-rows-1 h-[95%]">
+		<article class="flex flex-col justify-center mx-auto">
+			<h1
+				class="p-3 pl-0 duration-500 hover:bg-white hover:text-black hover:p-4 hover:mb-3 text-9xl"
+			>
+				Stellar
+			</h1>
+			<p>Stellar</p>
+		</article>
+		<section class="relative h-[95%]">
+			<Earth />
+		</section>
+	</div>
 </main>
