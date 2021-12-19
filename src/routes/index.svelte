08b825ed758f5d936ebd9da1c78e71d55885782d
Vivaan Verma <doublevcodes@gmail.com>
2021-12-19 00:21:25 +0000

    💄Add responsiveness and aesthetics
    The Earth and text were not wrapping on mobile, this commit makes it do that. I have also added some padding since the text touches the edge of the screen.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 
 <main class="h-screen text-white bg-black">
 	<Header />
-	<div class="grid grid-cols-2 grid-rows-1 h-[95%]">
+	<div class="grid grid-cols-1 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1 p-3 h-[95%]">
 		<article class="flex flex-col justify-center mx-auto">
 			<h1
 				class="p-3 pl-0 duration-500 hover:bg-white hover:text-black hover:p-4 hover:mb-3 text-9xl"
